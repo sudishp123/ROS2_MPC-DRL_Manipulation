9ec75eeeb9d4cece28a4f19cf2c4fed851da6e0b
Sudhish Poojary <sudishp123@gmail.com>
2026-06-02 13:21:48 -0300

    Adding Jetcobot urdf file

--- envs/dual_arm_env.py
+++ envs/dual_arm_env.py
@@ -50,6 +50,11 @@ class DualArmEnv(gym.Env):
 
         # ----- Spaces -------------------
         # Actions: taret joint angles in radians for all 12 joints
+        # Single-Arm (Adel et.al, 2024) = 16. In this case, action space = output of DRL which will be the parameters of the MPC
+        # 6 for setpoint control (S)
+        # 7 for control action/joint velocity minimization (R)
+        # 3 for collision avoidance term
+
         self.action_space = spaces.Box(
             low = self.ctrl_low.astype(np.float32),
             high = self.ctrl_high.astype(np.float32),
@@ -57,6 +62,14 @@ class DualArmEnv(gym.Env):
         )
 
         # Observations:
+        # Single-Arm (Adel et.al, 2024)
+        # 23 state variables:
+        # 6 for EE pose error variables (3 positions, 3 angles)
+        # 7 for joint velocities
+        # 7 for joint positions
+        # 3 for minimum distance between obstacle and 3 links (arm, forearm, hand)
+
+
         # 12 joint positions + 12 joint velocities
         # + left EE pos (3) + right EE pos (3)
         # + left EE quat (4) + right EE quat (4)
@@ -81,6 +94,33 @@ class DualArmEnv(gym.Env):
         self._renderer = None
         if render_mode == "rgb_array":
             self._renderer = mj.Renderer(self.model, height=480, width=640)
+
+        # ------- Resetting Environment (Gymnasium) --------
+        def reset(self, seed=None, options=None):
+            super().reset(seed=seed)
+            mujoco.mj_resetData(self.model, self.data)
+
+            # Setting both arms to home pose + small random perturbation
+            for i, jid in enumerate(self.left_jnt_ids + self.right_jnt_ids):
+                self.data.qpos[self.model.jnt_qposadr[jid]] = (
+                    self.home_qpos[i] + self.np_random.uniform(-0.1, 0.1)
+                )
+
+            # Set ctrl to ensure servos don't jitter on initial step
+            for i, aid in enumerate(self.all_act_ids):
+                jid = (self.left_jnt_ids + self.right_jnt_ids)[i]
+                self.data.ctrl[aid] = self.data.qpos[self.model.jnt_qposadr[jid]]
+
+            # Randomize Object and goal
+            self.data.qpos[self.model.jnt_qposadr[
+                mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_JOINT, "object_joint")
+            ]: self.model.jnt_qposadr[
+                mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_JOINT, "object_joint")
+            ] + 3] = [
+                self.np_random.uniform(-0.2, 0.1),
+                self.np_random.uniform(0.1, 0.3),
+            ]
+        
 
 
 
